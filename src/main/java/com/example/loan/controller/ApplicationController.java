fa5333ad43beabbb71bf3f40dcf892610017385b
leewg97 <leewg97@naver.com>
2023-04-05 19:12:01 +0900

    #5 : 대출 신청 등록 기능 구현 및 테스트 작성

---
+++ src/main/java/com/example/loan/controller/ApplicationController.java
@@ -0,0 +1,24 @@
+package com.example.loan.controller;
+
+import com.example.loan.dto.ApplicationDTO.Request;
+import com.example.loan.dto.ApplicationDTO.Response;
+import com.example.loan.dto.ResponseDTO;
+import com.example.loan.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/applications")
+public class ApplicationController extends AbstractController {
+
+    private final ApplicationService applicationService;
+
+    @PostMapping
+    public ResponseDTO<Response> create(@RequestBody Request request) {
+        return ok(applicationService.create(request));
+    }
+}
---
+++ src/main/java/com/example/loan/service/ApplicationService.java
@@ -0,0 +1,10 @@
+package com.example.loan.service;
+
+import com.example.loan.dto.ApplicationDTO.Request;
+import com.example.loan.dto.ApplicationDTO.Response;
+
+public interface ApplicationService {
+
+    Response create(Request request);
+
+}
